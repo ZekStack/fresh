e18db7d34025432e5d3d248dac6a16fdf1e93560
DrRandom <37899881+zekageri@users.noreply.github.com>
2026-07-17 08:45:18 +0200

    centralize Fresh buffer allocation

--- src/internal/FreshBuffer.h
+++ src/internal/FreshBuffer.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <Arduino.h>
-#include <esp_heap_caps.h>
+#include "FreshMemory.h"
 
-#include <cstdlib>
+#include <Arduino.h>
+
 #include <utility>
 
 class FreshBuffer {
@@ -39,41 +39,19 @@ class FreshBuffer {
 			return true;
 		}
 
-		_data = static_cast<uint8_t *>(heap_caps_malloc(size, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT));
-		if (_data != nullptr) {
-			_size = size;
-			_allocation = Allocation::HeapCaps;
-			return true;
+		_data = static_cast<uint8_t *>(FreshAllocate(size));
+		if (_data == nullptr) {
+			return false;
 		}
 
-		_data = static_cast<uint8_t *>(heap_caps_malloc(size, MALLOC_CAP_8BIT));
-		if (_data != nullptr) {
-			_size = size;
-			_allocation = Allocation::HeapCaps;
-			return true;
-		}
-
-		_data = static_cast<uint8_t *>(malloc(size));
-		if (_data != nullptr) {
-			_size = size;
-			_allocation = Allocation::Malloc;
-			return true;
-		}
-
-		return false;
+		_size = size;
+		return true;
 	}
 
 	void reset() {
-		if (_data != nullptr) {
-			if (_allocation == Allocation::Malloc) {
-				free(_data);
-			} else {
-				heap_caps_free(_data);
-			}
-		}
+		FreshDeallocate(_data);
 		_data = nullptr;
 		_size = 0;
-		_allocation = Allocation::None;
 	}
 
 	uint8_t *data() {
@@ -101,22 +79,13 @@ class FreshBuffer {
 	}
 
   private:
-	enum class Allocation : uint8_t {
-		None,
-		HeapCaps,
-		Malloc,
-	};
-
 	void moveFrom(FreshBuffer &&other) {
 		_data = other._data;
 		_size = other._size;
-		_allocation = other._allocation;
 		other._data = nullptr;
 		other._size = 0;
-		other._allocation = Allocation::None;
 	}
 
 	uint8_t *_data = nullptr;
 	size_t _size = 0;
-	Allocation _allocation = Allocation::None;
 };
